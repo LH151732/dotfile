efff1aebae50a9dc3f2ed697f43df004decb3c9a
HL <lh151732@gamil.com>
2024-12-02 01:03:59 +1100

    add colortheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,32 +1,29 @@
-return {
-  -- Highlight colors
+  return {
+  -- 使用 Mini Hipatterns，高亮颜色示例
   {
     "echasnovski/mini.hipatterns",
     event = "BufReadPre",
     opts = {},
   },
-  -- Tokyo Night theme
   {
-    "folke/tokyonight.nvim",
+    "catppuccin/nvim", -- 插件路径
+    name = "catppuccin",
     lazy = false,
-    priority = 1000,
+    priority = 1000, -- 保证 Catppuccin 优先加载
     opts = {
-      style = "storm", -- 其他选项: "night", "moon", "day"
-      transparent = true, -- 启用透明背景
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = {},
+      flavour = "mocha", -- 可选主题风格: latte, frappe, macchiato, mocha
+      transparent_background = true, -- 背景透明
+      term_colors = true, -- 终端的颜色与当前主题匹配
+      integrations = {
+        treesitter = true, -- 支持treesitter的语法高亮
+        nvimtree = true, -- 集成nvim-tree
+        cmp = true, -- 集成 nvim-cmp 的补全
+        gitsigns = true, -- 集成gitsigns
       },
-      -- 自定义高亮设置
-      on_highlights = function(hl, c)
-        hl.Comment = { fg = "#FFB7C5", italic = true } -- 樱粉色注释
-      end,
     },
     config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd([[colorscheme tokyonight]])
+      require("catppuccin").setup(opts) -- 加载带 opts 的设置
+      vim.cmd([[colorscheme catppuccin]]) -- 应用 colorscheme
     end,
   },
   -- Your existing Telescope configuration
