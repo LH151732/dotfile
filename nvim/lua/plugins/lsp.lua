f0b7b5db107332244751669b84a01be06dbf90de
XPS16 <lh151732@gamil.com>
2025-02-14 00:29:09 +1100

    New config with blink.cmp,<Tab> accepts suggestion

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -31,33 +31,75 @@ return {
       end, { silent = true, noremap = true, expr = true })
     end,
   },
-
-  -- 然后加载 blink 配置，不影响之前的设定
+  -- blink
   {
     "saghen/blink.cmp",
-    version = "v0.*",
-    dependencies = {
-      "L3MON4D3/LuaSnip",
-      version = "v2.*",
-    },
+    -- use a release tag to download pre-built binaries
+    version = "*",
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
-      snippets = {
-        expand = function(snippet)
-          require("luasnip").lsp_expand(snippet)
-        end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require("luasnip").jumpable(filter.direction)
-          end
-          return require("luasnip").in_snippet()
-        end,
-        jump = function(direction)
-          require("luasnip").jump(direction)
-        end,
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- See the full "keymap" documentation for information on defining your own keymap.
+      keymap = {
+        -- set to 'none' to disable the 'default' preset
+        preset = "default",
+
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+
+        -- disable a keymap from the preset
+        ["<C-e>"] = {},
+
+        ["<Tab>"] = {
+          function(cmp)
+            cmp.accept()
+          end,
+        },
+
+        -- show with a list of providers
+        ["<C-space>"] = {
+          function(cmp)
+            cmp.show({ providers = { "snippets" } })
+          end,
+        },
+
+        -- control whether the next command will be run when using a function
+        ["<C-n>"] = {
+          function(cmp)
+            if some_condition then
+              return
+            end -- runs the next command
+            return true -- doesn't run the next command
+          end,
+          "select_next",
+        },
+        -- optionally, separate cmdline keymaps
+        -- cmdline = {}
       },
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- Will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "mono",
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { "lsp", "path", "luasnip", "buffer" },
+        default = { "lsp", "path", "snippets", "buffer" },
       },
     },
+    opts_extend = { "sources.default" },
   },
 }
