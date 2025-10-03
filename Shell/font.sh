42965a0e96a94fbf06ed191dc5551e668eed5d4e
RTX <lh151732@gmail.com>
2025-10-03 20:23:19 +1000

    update: 修正fcitx5主题包名称

--- Shell/font.sh
+++ Shell/font.sh
@@ -1,1 +1,1 @@
-yay -S fcitx5-chinese-addons fcitx5-emoji ttf-twemoji ttf-joypixels noto-fonts noto-fonts-cjk noto-fonts-emoji fcitx5-configtool fcitx5 fcitx5-simple-theme
+yay -S fcitx5-chinese-addons fcitx5-emoji ttf-twemoji ttf-joypixels noto-fonts noto-fonts-cjk noto-fonts-emoji fcitx5-configtool fcitx5 simple-fcitx5-theme
