09dcc1da393b12a1a8e011875ccb1ef7716494ba
RTX <lh151732@gmail.com>
2025-10-03 19:08:56 +1000

    update: 扩展fcitx5字体安装包列表

--- Shell/font.sh
+++ Shell/font.sh
@@ -1,1 +1,1 @@
-yay -S fcitx5-chinese-addons fcitx5-emoji ttf-twemoji ttf-joypixels noto-fonts noto-fonts-cjk noto-fonts-emoji
+yay -S fcitx5-chinese-addons fcitx5-emoji ttf-twemoji ttf-joypixels noto-fonts noto-fonts-cjk noto-fonts-emoji fcitx5-configtool fcitx5 fcitx5-simple-theme
